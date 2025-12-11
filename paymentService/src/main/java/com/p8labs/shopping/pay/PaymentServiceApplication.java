b5f94ce916968c6e123c6fb2dd85d60d71636232
sungkuk ahn <33127270+rnrl1215@users.noreply.github.com>
2025-12-11 10:57:12 +0900

    Initial commit

---
+++ paymentService/src/main/java/com/p8labs/shopping/pay/PaymentServiceApplication.java
@@ -0,0 +1,11 @@
+package com.p8labs.shopping.pay;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PaymentServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ shippingService/src/main/java/com/p8labs/shopping/shipping/ShippingService.java
@@ -0,0 +1,10 @@
+package com.p8labs.shopping.shipping;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingService {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
