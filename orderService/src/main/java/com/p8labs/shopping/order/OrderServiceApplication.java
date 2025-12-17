86449d1bab2ee965f94e3414846a8da7c2669e17
sungkuk ahn <33127270+rnrl1215@users.noreply.github.com>
2025-12-17 17:07:01 +0900

    feat: 기본 설정 추가 - DB 유저 생성 - JPA 연동 - 주키퍼, 카프카 도커 추가.

--- orderService/src/main/java/com/p8labs/shopping/order/OrderServiceApplication.java
+++ orderService/src/main/java/com/p8labs/shopping/order/OrderServiceApplication.java
@@ -1,4 +1,4 @@
-package com.p8labs.shopping.common;
+package com.p8labs.shopping.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- paymentService/src/main/java/com/p8labs/shopping/pay/PaymentServiceApplication.java
+++ paymentService/src/main/java/com/p8labs/shopping/pay/PaymentServiceApplication.java
@@ -1,11 +1,12 @@
 package com.p8labs.shopping.pay;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class PaymentServiceApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(PaymentServiceApplication.class, args);
     }
 }
